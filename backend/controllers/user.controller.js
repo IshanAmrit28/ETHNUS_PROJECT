7011d8defea8fed9ac0d236f732fee96b1d28a99
Ishan Amrit Srivastava <98459119+IshanAmrit28@users.noreply.github.com>
2025-04-11 05:36:53 +0530

    frontend added, backend few changes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import getDataUri from "../utils/datauri.js";
-import cloudinary from "../utils/cloudinary.js";
+// import getDataUri from "../utils/datauri.js";
+
 
 export const register = async (req, res) => {
     try {
@@ -14,9 +14,9 @@ export const register = async (req, res) => {
                 success: false
             });
         };
-        const file = req.file;
-        const fileUri = getDataUri(file);
-        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        // const file = req.file;
+        // const fileUri = getDataUri(file);
+        // const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
 
         const user = await User.findOne({ email });
         if (user) {
@@ -33,9 +33,9 @@ export const register = async (req, res) => {
             phoneNumber,
             password: hashedPassword,
             role,
-            profile:{
-                profilePhoto:cloudResponse.secure_url,
-            }
+            // profile:{
+            //     profilePhoto:cloudResponse.secure_url,
+            // }
         });
 
         return res.status(201).json({
@@ -115,10 +115,15 @@ export const updateProfile = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, bio, skills } = req.body;
         
-        const file = req.file;
-        // cloudinary ayega idhar
-        const fileUri = getDataUri(file);
-        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        if(!fullname||!email||!phoneNumber||!bio||!skills){
+            return res.status(400).json({
+                message:"something is missing",
+                success:false});
+        };
+        // const file = req.file;
+        // // cloudinary ayega idhar
+        // const fileUri = getDataUri(file);
+        // const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
 
 
 
@@ -143,10 +148,10 @@ export const updateProfile = async (req, res) => {
         if(skills) user.profile.skills = skillsArray
       
         // resume comes later here...
-        if(cloudResponse){
-            user.profile.resume = cloudResponse.secure_url // save the cloudinary url
-            user.profile.resumeOriginalName = file.originalname // Save the original file name
-        }
+        // if(cloudResponse){
+        //     user.profile.resume = cloudResponse.secure_url // save the cloudinary url
+        //     user.profile.resumeOriginalName = file.originalname // Save the original file name
+        // }
 
 
         await user.save();
---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Button } from "../ui/button";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { LogOut, User2 } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const user = false;
+
+  return (
+    <div className="bg-blue-900">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16 px-4">
+        <div>
+          <h1 className="text-2xl font-bold text-white">
+            Job<span className="text-[#F83002]">Portal</span>
+          </h1>
+        </div>
+
+        <div className="flex items-center gap-12">
+          <ul className="flex font-medium items-center gap-5 text-white">
+            <li className="cursor-pointer">Home</li>
+            <li className="cursor-pointer">Jobs</li>
+            <li className="cursor-pointer">Browse</li>
+          </ul>
+
+          {!user ? (
+            <div className="flex items-center gap-3">
+              <Link to="/login">
+                <Button variant="outline">Login</Button>
+              </Link>
+              <Link to="/signup">
+                <Button>Signup</Button>
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                  </Avatar>
+                </PopoverTrigger>
+
+                <PopoverContent className="w-80 p-4">
+                  {/* Profile Info */}
+                  <div className="flex items-center gap-4 mb-4">
+                    <Avatar>
+                      <AvatarImage
+                        src="https://github.com/shadcn.png"
+                        alt="@shadcn"
+                      />
+                    </Avatar>
+                    <div>
+                      <h5 className="font-medium">Ishan Amrit Srivastava</h5>
+                      <p className="text-sm text-muted-foreground">
+                        MERN STACK DEVELOPER
+                      </p>
+                    </div>
+                  </div>
+
+                  {/* Options */}
+                  <div className="flex flex-col gap-2">
+                    <Button
+                      variant="link"
+                      className="flex items-center gap-2 w-fit text-gray-700"
+                    >
+                      <User2 className="h-4 w-4" />
+                      Profile
+                    </Button>
+
+                    <Button
+                      variant="link"
+                      className="flex items-center gap-2 w-fit text-gray-700"
+                    >
+                      <LogOut className="h-4 w-4" />
+                      Logout
+                    </Button>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
